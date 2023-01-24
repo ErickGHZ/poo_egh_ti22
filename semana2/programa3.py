3f7c0d15e1bfff6a5697013c518d5f3e2efc9790
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-01-24 14:31:28 +0000

    Agregue programa3.py en la carpeta semana2

---
+++ semana2/programa3.py
@@ -0,0 +1,11 @@
+"""
+    Programa 3
+    Nombre: Erick Gutierrez Hernandez
+    Fecha: 24/01/2023
+    Descripcion:  En este programa veremos como realizar impresiones de operaciones aritmeticas de la forma tradicional "print" y con la funcion format
+"""
+variable1 = 10  #  variable para almacenar una cadena de caracteres
+variable2 = 9  #  variable para almacenar una cadena de caracteres
+print(variable1+variable2)  #  imprime la suma de dos variable
+print("{} + {} = {}".format(variable1,variable2,variable1+variable2))  #  imprime con la funcion format las variables, ademas realiza la suma de estas 2 variables y muestra el resultado de estas 2
+
