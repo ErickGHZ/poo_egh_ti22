1b95fdebd113f46af74b2441a098cbe8f92abd4c
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-01-30 14:56:06 +0000

    Agregue el archivo programa4.py en la carpeta semana3 la cual igual es nueva :D

--- semana2/programa3.py
+++ semana2/programa3.py
@@ -8,5 +8,6 @@ variable1 = 10  #  variable para almacenar una cadena de caracteres
 variable2 = 5  #  variable para almacenar una cadena de caracteres
 print(variable1+variable2)  #  imprime la suma de dos variable
 print("{} + {} = {}".format(variable1,variable2,variable1+variable2))  #  imprime con la funcion format las variables, ademas realiza la suma de estas 2 variables y muestra el resultado de estas 2
+print("{} - {} = {}".format(variable1,variable2,variable1-variable2))  #  imprime con la funcion format las variables, ademas realiza la resta de estas 2 variables y muestra el resultado de estas 2
 print("{} = {} * {}".format(variable1*variable2,variable2,variable1))  #  imprime con la funcion format las variables, ademas realiza la multiplicacion de estas 2 variables y muestra el resultado de estas 2 con otro orden 
 print("El resultado de dividir {} / {} es {}".format(variable1,variable2,variable1/variable2))  #  imprime con la funcion format texto y variables, ademas realiza la divicion de 2 variables y muestra el resultado de estas 2  
---
+++ semana3/programa4.py
@@ -0,0 +1,10 @@
+"""
+"""
+numero1 = int(input("Numero 1: "))  #  Se asigna una cadena de texto a una variable la cual es convertida en un "int" para que no genere un fallo al querer intentar sumar un int y un string
+numero2 = 10  #  Se asigna un valor tipo int a la variable
+suma = numero1 + numero2  #  Se realiza una suma de 2 variables y la guardan el resultado de estas en otra variable
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+potencia = numero1**numero2
+print("La suma de estos dos numeros es:", suma , "\nLa resta de estos dos numeros es:", resta , "\nLa multiplicacion de estos dos numeros es:", multiplicacion , "\nLa division de estos dos numeros es:", division , "\nLa potencia de estos dos numeros es:", potencia )
