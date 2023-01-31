615f53498c0acf1dc2568dec3147f9fa777c69d6
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-01-31 22:09:47 +0000

    Agregue comentarios y edite el readme

--- semana3/tarea0.py
+++ semana3/tarea0.py
@@ -3,10 +3,10 @@ Programa de Python para calcular y medir el perímetro y área de cualquier tri�
 """
 import math  #  importa la libreria math para poder realizar una raiz cuadrada
 
-lado_a = float(input("Ingresa el valor del primer lado del triangulo: "))
-lado_b = float(input("Ingresa el valor del segundo lado del triangulo: "))
-lado_c = float(input("Ingresa el valor del tercer lado del trianulo: "))
-perimetro = lado_a + lado_b + lado_c
-s = perimetro/2
-area = math.sqrt(s * (s-lado_a) * (s-lado_b) * (s-lado_c))
-print("El perimetro del triangulo es:", perimetro , "\nEl area del triangulo es:", area)
+lado_a = float(input("Ingresa el valor del primer lado del triangulo: "))  #  Se asigna una cadena de texto a una variable la cual es convertida en un "int" 
+lado_b = float(input("Ingresa el valor del segundo lado del triangulo: "))  #  Se asigna una cadena de texto a una variable la cual es convertida en un "int" 
+lado_c = float(input("Ingresa el valor del tercer lado del trianulo: "))  #  Se asigna una cadena de texto a una variable la cual es convertida en un "int" 
+perimetro = lado_a + lado_b + lado_c   #  Se asigna el resultado de la suma de dos variables a una variable 
+s = perimetro/2  #  Se asigna el resultado de la divicion de dos varibales a un variable
+area = math.sqrt(s * (s-lado_a) * (s-lado_b) * (s-lado_c))  #  se asgina el resultado de una expresion matematica a una varibale
+print("El perimetro del triangulo es:", perimetro , "\nEl area del triangulo es:", area)  #  se imprime una cadena de texto junto a unas variables
