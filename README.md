cee508e74a72e371662c9469e68bf5df968b0004
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-02-01 00:45:32 +0000

    Agregue el archivo programa6.py y edite el archivo README.md

--- README.md
+++ README.md
@@ -10,8 +10,8 @@
 |3|programa2.py|Concatenaciones de variables e identificar errores|
 |4|programa3.py|Operaciones aritemeticas y como mostrarlas con la funcion format|
 |5|programa4.py|Convertir un string a un int con casting|
-|6|programa5.py|Cacular e imprimir el perimetro y area de cualquier triangulo|
-
+|6|programa5.py|Calcular e imprimir el perimetro y area de cualquier triangulo|
+|7|programa7.py|Calcular e imprimir el perimetro y area de un circulo y un cuadrado|
 
 
 
--- semana3/programa6.py
+++ semana3/programa6.py
@@ -5,9 +5,15 @@
     Descripción: Programa para calcular el perimetro y area de un circulo y un cuadrado
 """
 PI = 3.1416
-radio =  float(input("Ingrese el valor del radio del circulo: "))  #  Se asigna una cadena de texto a una variable la cual es convertida en un "int"
-diametro = 2 * radio
-perimetro = PI * diametro
-area = PI * (radio ** 2)
-radiocuadrado = radio**2
-print("El perimetro del dirculo es", perimetro , "\nEl area del circulo es:", area)  #  se imprime una cadena de texto junto a unas variables
+radio = float(input("Ingrese el valor del radio del circulo: "))  #  Se asigna una cadena de texto a una variable la cual es convertida en un "int"
+diametro = 2 * radio  #  multiplica un variable y la guarda en otra variable
+perimetroCirculo = PI * diametro  #  multiplica una variable por otra y lo guarda en otra variable
+areaCirculo = PI * (radio ** 2)  #  se asgina el resultado de una expresion matematica a una varibale
+print("El perimetro del circulo es:", perimetroCirculo , "\nEl area del circulo es:", areaCirculo)  #  se imprime una cadena de texto junto a unas variables
+
+lado = float(input("Ingresa el valor del lado de un cuadrado: "))  #  Se asigna una cadena de texto a una variable la cual es convertida en un "int"
+perimetroCuadrado = 4 * lado  #  multiplica un variable y la guarda en otra variable
+areaCuadrado  = lado * lado  #  multiplica una variable por otra y lo guarda en otra variable
+print("El perimetro del cuadrado es:", perimetroCuadrado , "\nEl area del cuadrado es:", areaCuadrado)  #  se imprime una cadena de texto junto a unas variables
+
+
