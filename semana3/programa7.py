b37caf874d1cd8c97ddc620ac593e3ae767330fd
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-02-07 14:31:59 +0000

    agregue la versión 1.1 del archivo programa7.py

--- semana3/programa7.py
+++ semana3/programa7.py
@@ -6,7 +6,9 @@
 """
 numero1 = int(input("Ingresa un numero entero:"))  #  Asigan el valor de una cadena de texto a una variable y la convierte en un entero
 numero2 = int(input("Ingresa otro numero entero:"))  #  Asigan el valor de una cadena de texto a una variable y la convierte en un entero
-if numero1>=numero2:  #  realiza una comparacion de dos variables
+if numero1>numero2:  #  realiza una comparacion de dos variables
     print(numero1)  #  muestra una cadena de texto si la comparacion anterior es "true"
+elif numero1 == numero2:  #  realiza otra comparacion en caso de que la anterior sea falsa
+    print(None)  #  muestra una cadena de texto si la comparacion anterior es "flase" y esta es verdadera
 else:  #  asigna una accion en caso de que la comparacion sea falsa 
-    print(numero2)  #  muestra una cadena de texto si la comparacion anterior es "flase"
+     print(numero2)  #  muestra una cadena de texto si la comparacion anterior es "flase"
